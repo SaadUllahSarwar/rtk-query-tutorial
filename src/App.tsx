6ed02c4130e2e063268d735eca025964d180efe8
Saad Sarwar <saad.sarwar@heuristicsol.com>
2022-09-07 21:02:21 +0500

    all changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
 
 import './App.css';
+import { useContactsQuery,useContactQuery,useAddContactMutation,useUpdateContactMutation,useDeleteContactMutation} from './services/contactsApi'
 
 function App() {
+  const {data,error,isLoading,isFetching,isSuccess} = useContactsQuery();
   return (
     <div className="App">
-      
-            React Redux
-        
+      <h1>React Redux Query Toolkit</h1>
+      {isLoading && <h2>Data is Loading ...</h2>}
+      {isFetching && <h2>... Fetching</h2>}
+      {error && <h2>Something Went Wrong</h2>}
+      {isSuccess && (
+        <div>{data?.map( contact=> {
+          return <div className="data" key={contact.id}>
+            <span>{contact.name}</span>
+            <br/> 
+            <span><ContactDetail   id={contact.id}/> </span>
+          </div>
+        })}
+        </div>
+      )}     
+      <div>
+        <AddContact />
+        </div>   
     </div>
   );
 }
-
+ export const ContactDetail = ({id} : {id:string}) => {
+   const {data} = useContactQuery(id); 
+      return (
+        <pre>{JSON.stringify(data)}</pre>
+      )
+ }
+ export const AddContact = () => {
+   const [addContact] = useAddContactMutation();
+   const contact = {
+     "id" : "4",
+     "name" : "umar",
+     "email" : "umar@gmail.com"
+   }
+   const addHandler = async() => {
+      await addContact(contact);
+   }
+      return (
+     <>
+     <button onClick={addHandler}>Add Contact</button>
+     </>
+   )
+   
+ }
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import {store} from './store'
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
 
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
-    
+    <Provider store={store}>
       <App />
-  
-  </React.StrictMode>
+      </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')!
 );
 
 // If you want to start measuring performance in your app, pass a function
